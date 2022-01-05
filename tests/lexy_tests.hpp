ead0878e32423ebfd97ea622affc65bb0f59155c
MetalcoreLoli <deadpool1118@gmail.com>
2022-01-05 17:01:58 +0300

    add gmock

--- tests/lexy_tests.hpp
+++ tests/lexy_tests.hpp
@@ -1,18 +1,28 @@
 #ifndef __LEXY_TESTS__
 #define __LEXY_TESTS__
 
-#include "loliLang/daphnie.h"
-#include "loliLang/lexy.h"
 #include <cmath>
 #include <exception>
+#include <stack>
 #include <stdexcept>
 #include <string>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <loliLang/common.h>
+#include <path_utils.h>
 
 class LexyTests : public ::testing::Test {
     protected:
         loli::Lexy _lex{};
+};
+
+class MockCaller : public loli::ICallable {
+    public:
+        MOCK_METHOD(
+                loli::GenericLink, 
+                callLambdaExpression, 
+                (loli::LambdaExpression&, std::stack<loli::Expression*>&),
+                (override));
 };
 
 TEST_F (LexyTests, visitBinaryExpression_WithOneAddOne_ReturnsTwo) {
@@ -51,5 +61,26 @@ TEST_F (LexyTests, visitIfExpression_WithOneSubOneLtOne_ReturnsTrue) {
 
     //assert
     ASSERT_TRUE(result);
+}
+
+
+TEST_F (LexyTests, visitLambdaExpression_WithValidOnePlusOne_ReturnsTwo) {
+    using ::testing::Return;
+    auto code = "add => 1 + 1;";
+    MockCaller caller {};
+    std::stack<loli::Expression*> _emptyStack{};
+    EXPECT_CALL(caller,
+                callLambdaExpression(testing::_, testing::_))
+            .Times(1)
+            .WillOnce(Return(loli::newLink<float>(2.0f)));
+
+    //act
+    auto func =
+        loli::unwrap<void, loli::LambdaExpression>(loli::Daphnie{code}.growTree()->visit(&_lex));
+
+    auto result = loli::unwrap<void, float>(func.call(&caller, _emptyStack));
+
+    //assert
+    ASSERT_EQ(2.0f, result);
 }
 #endif //__LEXY_TESTS__
