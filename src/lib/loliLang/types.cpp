c2f62fb0ce11a28329c2a955239c3bd23a2bb732
MetalcoreLoli <deadpool1118@gmail.com>
2022-02-06 00:40:19 +0300

    add types und typechecker

---
+++ src/lib/loliLang/types.cpp
@@ -0,0 +1,58 @@
+#include "loliLang/types.h"
+#include "loliLang/utils.h"
+
+loli::ReturnResult loli::TypeChecker::visitBinaryExpression (BinaryExpression& value) {
+//    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitBinaryExpression");
+//    return ReturnResult::Empty();
+    auto left   = value.left()->visit(this);
+    auto right  = value.right()->visit(this);
+    if (left.TypeHashCode() != right.TypeHashCode()) {
+        throw std::runtime_error {"type of left value is not the same as type of right value"};
+    }
+    return left;
+} 
+
+loli::ReturnResult loli::TypeChecker::visitNumberExpression (NumberExpression& value) {
+    return {utils::newLink<FloatType>(), typeid(FloatType).hash_code()};
+} 
+loli::ReturnResult loli::TypeChecker::visitLambdaExpression (LambdaExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitLambdaExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitIdentifierExpression (IdentifierExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitIdentifierExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitStringExpression (StringExpression& value) {
+    return {utils::newLink<StringType>(), typeid(StringType).hash_code()};
+} 
+loli::ReturnResult loli::TypeChecker::visitIfExpression (IfExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitIfExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitGroupingExpression (GroupingExpression& value) {
+    return value.expression()->visit(this);
+} 
+loli::ReturnResult loli::TypeChecker::visitBoolExpression (BoolExpression& value) {
+    return {utils::newLink<BoolType>(), typeid(BoolType).hash_code()};
+} 
+loli::ReturnResult loli::TypeChecker::visitUnaryExpression (UnaryExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitUnaryExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitClassExpression (ClassExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitClassExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitBodyExpression (BodyExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitBodyExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitForExpression (ForExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitForExpression");
+    return ReturnResult::Empty();
+} 
+loli::ReturnResult loli::TypeChecker::visitCallExpression (CallExpression& value) {
+    utils::ThrowHelper::Throw_NotImplemented("loli::TypeChecker::visitCallExpression");
+    return ReturnResult::Empty();
+} 
---
+++ src/lib/loliLang/types.h
@@ -0,0 +1,79 @@
+#ifndef __LOLI_TYPES__
+#define __LOLI_TYPES__
+#include <functional>
+#include <string>
+#include <map>
+#include <string_view>
+
+#include "loliLang/expression.h"
+#include "loliLang/utils.h"
+
+namespace loli {
+    template <typename T>
+    class TypeVTable {
+        using BinaryOpFunction = std::function <ReturnResult(T, T)>;
+        using BinaryOpsTable = std::map <std::string_view, BinaryOpFunction>;
+        BinaryOpsTable _binaryOps {};
+
+        public:
+            TypeVTable(){}
+            BinaryOpsTable BinaryOps () const {
+                return const_cast<BinaryOpsTable&>(_binaryOps);
+            }
+            
+            BinaryOpFunction BinaryOp (std::string_view sv)  {
+                if (_binaryOps.contains(sv)) return _binaryOps[sv];
+                else {
+                    utils::ThrowHelper::Throw_OperationIsNotImplementedForType (std::string(sv), typeid(T).name());
+                }
+            }
+    };
+
+    template <typename T>
+    class Type {
+        protected:
+            TypeVTable<T> _vTable{};
+
+        public:
+            Type (){}
+            virtual TypeVTable<T>& VTable() const {return const_cast<TypeVTable<T>&>(_vTable);}
+
+    };
+
+
+    class FloatType : public Type<float> {
+        public:
+            FloatType () {
+                _vTable.BinaryOps().insert({"+", [](float a, float b) -> ReturnResult {return {newLink<float>(a+b), typeid(float).hash_code()};}});
+                _vTable.BinaryOps().insert({"-", [](float a, float b) -> ReturnResult {return {newLink<float>(a-b), typeid(float).hash_code()};}});
+                _vTable.BinaryOps().insert({"*", [](float a, float b) -> ReturnResult {return {newLink<float>(a*b), typeid(float).hash_code()};}});
+                _vTable.BinaryOps().insert({"/", [](float a, float b) -> ReturnResult {return {newLink<float>(a/b), typeid(float).hash_code()};}});
+                _vTable.BinaryOps().insert({"<", [](float a, float b) -> ReturnResult {return {newLink<float>(a<b), typeid(float).hash_code()};}});
+                _vTable.BinaryOps().insert({">", [](float a, float b) -> ReturnResult {return {newLink<float>(a>b), typeid(float).hash_code()};}});
+                _vTable.BinaryOps().insert({"!=", [](float a, float b) -> ReturnResult {return {newLink<bool>(a!=b), typeid(bool).hash_code()};}});
+                _vTable.BinaryOps().insert({"==", [](float a, float b) -> ReturnResult {return {newLink<bool>(a==b), typeid(bool).hash_code()};}});
+            }
+    };
+
+    class BoolType : public Type<bool> {};
+    class StringType : public Type<std::string> {};
+
+
+    class TypeChecker : public IVisitor {
+        public:
+            ReturnResult visitBinaryExpression (BinaryExpression& value) override;
+            ReturnResult visitNumberExpression (NumberExpression& value) override;
+            ReturnResult visitLambdaExpression (LambdaExpression& value) override;
+            ReturnResult visitIdentifierExpression (IdentifierExpression& value) override;
+            ReturnResult visitStringExpression (StringExpression& value) override;
+            ReturnResult visitIfExpression (IfExpression& value) override;
+            ReturnResult visitGroupingExpression (GroupingExpression& value) override;
+            ReturnResult visitBoolExpression (BoolExpression& value) override;
+            ReturnResult visitUnaryExpression (UnaryExpression& value) override;
+            ReturnResult visitClassExpression (ClassExpression& value) override;
+            ReturnResult visitBodyExpression (BodyExpression& value) override;
+            ReturnResult visitForExpression (ForExpression& value) override;
+            ReturnResult visitCallExpression (CallExpression& value) override;
+    };
+}
+#endif // __LOLI_TYPES__
