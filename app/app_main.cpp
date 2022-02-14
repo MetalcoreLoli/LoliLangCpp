dff8157464018bf3bc42b8b5768a7dee79ea1732
MetalcoreLoli <deadpool1118@gmail.com>
2022-02-14 17:53:14 +0300

    fix fibHelper

--- app/app_main.cpp
+++ app/app_main.cpp
@@ -12,7 +12,7 @@ int main () {
     loli::ASTAsString ast;
     loli::LoliInter inter {};
 
-    auto fibHelper  = inter.AnswerOn("fibHelper a b n => if (0 < n) fibHelper (a + b) a (1 - n) else a");
+    auto fibHelper  = inter.AnswerOn("fibHelper a b n => if (0 < n) fibHelper (a + b) a (n - 1) else a");
     auto fib        = inter.AnswerOn("fib n => fibHelper 0 1 n");
     auto helloStr = inter.AnswerOn("fib 10").As<float>();
 
