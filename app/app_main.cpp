fdf1ff710fd486ba6e1a6c4ae3ac5bac6c850ce1
MetalcoreLoli <deadpool1118@gmail.com>
2022-02-15 18:21:54 +0300

    impl reading code from file

--- app/app_main.cpp
+++ app/app_main.cpp
@@ -2,19 +2,76 @@
 // Created by danielf on 05.01.2022.
 //
 
+#include "loliLang/daphnie.h"
+#include <cassert>
+#include <cstddef>
+#include <ostream>
+#include <vector>
 #define LOLI_LOG_STD
 #include <iostream>
+#include <fstream>
 #include <loliLang/common.h>
 #include <string>
 
+#define ANSII_CLEAR_SCR "\x1b[27J"
 
-int main () {
-    loli::ASTAsString ast;
+struct STDOutWrite : public loli::IWriter {
+    void Write (const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+std::size_t replace_all(std::string& inout, std::string_view what, std::string_view with)
+{
+    std::size_t count{};
+    for (std::string::size_type pos{};
+         inout.npos != (pos = inout.find(what.data(), pos, what.length()));
+         pos += with.length(), ++count) {
+        inout.replace(pos, what.length(), with.data(), with.length());
+    }
+    return count;
+}
+
+int main (int argc, char** argv) {
     loli::LoliInter inter {};
 
-    auto fibHelper  = inter.AnswerOn("fibHelper a b n => if (0 < n) fibHelper (a + b) a (n - 1) else a");
-    auto fib        = inter.AnswerOn("fib n => fibHelper 0 1 n");
-    auto helloStr = inter.AnswerOn("fib 10").As<float>();
+    if (argc < 2) {
+        std::cout << "the source code file was not pass throuht" << std::endl;
+        return -1;
+    }
+    loli::ASTAsString ast{};
+    
+    std::ifstream sourceCode(argv[1]);
+    std::string line;
+    std::string code;
+    std::vector<std::string> lines {};
+    while (std::getline (sourceCode, line)) {
+        if (!line.empty()) {
+            if (line.substr(0, 4) == "    ") {
+               code.append("t0R3pl4c3"); 
+            }
+            code.append(line).append("\n");
+        }
+    }
 
-    std::cout << helloStr << std::endl;
+    auto c = replace_all(code, "\nt0R3pl4c3", "");
+    line = "";
+    for (auto& cc : code) {
+        if (cc != '\n')  {
+            line += cc;
+        } else {
+            lines.push_back(line);
+            line = "";
+        }
+    }    
+    {
+        for (auto& line : lines) {
+            loli::Daphnie d {line};
+            std::cout << d.growTree()-> visit(&ast).Unwrap<std::string>() << std::endl;
+            auto res = inter.AnswerOn(line);
+            if (res.RawResult().TypeHashCode() == typeid(float).hash_code()) {
+                std::cout << res.As<float>() << std::endl;
+            }
+        }
+    }
 }
